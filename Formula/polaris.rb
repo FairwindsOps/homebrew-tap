93dee148cda872d9ce279ff095e2d77a9e158783
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-16 11:17:58 -0400

    Brew formula update for polaris version 0.0.1

--- Formula/polaris.rb
+++ Formula/polaris.rb
@@ -3,7 +3,7 @@ class Polaris < Formula
   homepage ""
   url "https://github.com/reactiveops/polaris/releases/download/0.0.1/polaris_0.0.1_Darwin_x86_64.tar.gz"
   version "0.0.1"
-  sha256 "b6210b1fb110a5cdebc83135bb520e13758cbccd3fedb81344840a9299353581"
+  sha256 "ffc3cdd7e139fc4be17761b90e739ef20063f923b41fdf6c32faf889af585fda"
 
   def install
     bin.install "polaris"
