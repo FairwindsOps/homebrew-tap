9a868280f300c3d2b4f0c0842feb378916f67104
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-05 12:24:30 -0400

    Brew formula update for polaris version 0.1.5

--- Formula/polaris.rb
+++ Formula/polaris.rb
@@ -1,9 +1,9 @@
 class Polaris < Formula
   desc "Open Source Best Practices for Kubernetes"
   homepage ""
-  url "https://github.com/reactiveops/polaris/releases/download/0.1.4/polaris_0.1.4_Darwin_x86_64.tar.gz"
-  version "0.1.4"
-  sha256 "b1289032eaa1538bebce1bca1c258f75adeb5fffb22d99c809f22e52a31a79bf"
+  url "https://github.com/reactiveops/polaris/releases/download/0.1.5/polaris_0.1.5_Darwin_x86_64.tar.gz"
+  version "0.1.5"
+  sha256 "7120b078c2a8669e9a7d7286548b05c46e2e139bd21c08e8a6b5164781aaea8c"
 
   def install
     bin.install "polaris"
