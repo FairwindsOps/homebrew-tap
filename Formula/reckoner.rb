3381e457226be5bf12702dcf2917b340bb4b3075
Bobby Brennan <bobby.brennan@gmail.com>
2019-07-25 16:32:11 +0000

    Rename ReactiveOps to Fairwinds

--- Formula/reckoner.rb
+++ Formula/reckoner.rb
@@ -1,7 +1,7 @@
 class Reckoner < Formula
   desc "Tool to simplify management and installation of multiple Helm chart releases."
-  homepage "https://github.com/reactiveops/reckoner"
-  url "https://github.com/reactiveops/reckoner/releases/download/v1.1.6/reckoner-darwin"
+  homepage "https://github.com/FairwindsOps/reckoner"
+  url "https://github.com/FairwindsOps/reckoner/releases/download/v1.1.6/reckoner-darwin"
   version "1.1.6"
   sha256 "8651efdaf4ebd879e7ef647c6549c1f3bf908632ce0b26bb5ce3dd66e285b9ae"
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # homebrew-tap [![Powered By: GoReleaser](https://img.shields.io/badge/powered%20by-goreleaser-green.svg?style=flat-square)](https://github.com/goreleaser)
 
-Homebrew Formulae for ReactiveOps binaries, powered by @goreleaser.
+Homebrew Formulae for Fairwinds binaries, powered by @goreleaser.
