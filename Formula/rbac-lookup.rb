ee94013a0323fcc4fafe93e66779ae193dbb41b6
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-23 10:26:22 -0600

    Brew formula update for rbac-lookup version v0.6.2

--- Formula/rbac-lookup.rb
+++ Formula/rbac-lookup.rb
@@ -1,9 +1,9 @@
 class RbacLookup < Formula
   desc "Reverse Lookup for Kubernetes RBAC"
   homepage ""
-  url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.1/rbac-lookup_0.6.1_Darwin_x86_64.tar.gz"
-  version "0.6.1"
-  sha256 "a37c59b073efa677a2eb4da1338cc0d9cf3fa28bc1b224e0a9186d6ef568d252"
+  url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.2/rbac-lookup_0.6.2_Darwin_x86_64.tar.gz"
+  version "0.6.2"
+  sha256 "162b2ae171ed9a20c68cffb610750dd230964df9571296257950f812ce7ff035"
 
   def install
     bin.install "rbac-lookup"
