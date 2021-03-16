95d71ab68c67f56e64d48866d889416c1067ce1a
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-16 07:36:54 -0600

    Brew formula update for polaris version 3.2.0

--- Formula/polaris.rb
+++ Formula/polaris.rb
@@ -2,24 +2,24 @@
 class Polaris < Formula
   desc "Open Source Best Practices for Kubernetes"
   homepage ""
-  version "3.1.6"
+  version "3.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/FairwindsOps/polaris/releases/download/3.1.6/polaris_3.1.6_darwin_amd64.tar.gz"
-    sha256 "6c7620e3e70380b336858d5d2cbc650531a00886f625cad81a198f0224aa2f96"
+    url "https://github.com/FairwindsOps/polaris/releases/download/3.2.0/polaris_3.2.0_darwin_amd64.tar.gz"
+    sha256 "ef6d1e84a51be517f7c76b2f0da8be72ee8ada3477792d3b13fc1d197bf049e9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/FairwindsOps/polaris/releases/download/3.1.6/polaris_3.1.6_linux_amd64.tar.gz"
-      sha256 "ede385beb24d34350a6dee603c1a0c2359e43722a77c2517681fbd05a859077c"
+      url "https://github.com/FairwindsOps/polaris/releases/download/3.2.0/polaris_3.2.0_linux_amd64.tar.gz"
+      sha256 "e37368dec56a2f97e0de0e036d498a7f5508f15e153cd7f56fea997ebb56dae4"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/FairwindsOps/polaris/releases/download/3.1.6/polaris_3.1.6_linux_arm64.tar.gz"
-        sha256 "48689486dba0aa6aa87c43e79dd4d92c464280b5ef8d67b89e15931a602c01ce"
+        url "https://github.com/FairwindsOps/polaris/releases/download/3.2.0/polaris_3.2.0_linux_arm64.tar.gz"
+        sha256 "d2ecfdeb600e98b33a43c7d268eaa240b0d9b0b791e3aaf94a5c94a7da318525"
       else
-        url "https://github.com/FairwindsOps/polaris/releases/download/3.1.6/polaris_3.1.6_linux_armv6.tar.gz"
-        sha256 "62013a2e4cef5dc86206ba8ee5ffe12afb76a8094600ce687822f2eba89c6032"
+        url "https://github.com/FairwindsOps/polaris/releases/download/3.2.0/polaris_3.2.0_linux_armv6.tar.gz"
+        sha256 "3624849bca02cfbf43f3c181ada0702fda4102d86e4cd71d98d448191f8a84df"
       end
     end
   end
