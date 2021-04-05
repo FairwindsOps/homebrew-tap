c3802c6bf8b835f03f4ea1b55d42cf743623dbf9
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-05 10:52:06 -0600

    Brew formula update for rbac-lookup version v0.6.4

--- Formula/homebrew-tap.rb
+++ Formula/homebrew-tap.rb
@@ -5,21 +5,21 @@
 class HomebrewTap < Formula
   desc "Reverse Lookup for Kubernetes RBAC"
   homepage "https://github.com/FairWindsOps/rbac-lookup"
-  version "0.6.3"
+  version "0.6.4"
   license "apache-2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.3/rbac-lookup_0.6.3_Darwin_x86_64.tar.gz"
-    sha256 "aa3c825d3e973fe2ed013189fea918570b1e7a1e818506f484eb2f53c08939f6"
+    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.4/rbac-lookup_0.6.4_Darwin_x86_64.tar.gz"
+    sha256 "7d914297d9ceede8f07d196d56de788ede081e844c17b435ea04092541c15b1e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.3/rbac-lookup_0.6.3_Linux_x86_64.tar.gz"
-    sha256 "6d8bb2664d27d3f104219da016665ee111ea2b30eb7a0bd0fc0e66cc50e3291d"
+    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.4/rbac-lookup_0.6.4_Linux_x86_64.tar.gz"
+    sha256 "9f6f63b2ee6f5420530b6aa589b4c8c4a4685bf87447286881d37afdd80a7fb2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.3/rbac-lookup_0.6.3_Linux_arm64.tar.gz"
-    sha256 "4d1d0f7549832623b236c952671196f16b14ae59c9faa3300e32017b38ca47bb"
+    url "https://github.com/FairwindsOps/rbac-lookup/releases/download/v0.6.4/rbac-lookup_0.6.4_Linux_arm64.tar.gz"
+    sha256 "df85119b39a22aee0d9472d5c48210943329cbd3f878aec159fa1b5ccd6818a1"
   end
 
   def install
