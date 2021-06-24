0c9f32ccaf6e74f5a555dc0df1ce217e69237da3
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-24 10:35:32 -0400

    Brew formula update for nova version 2.3.2

--- Formula/nova.rb
+++ Formula/nova.rb
@@ -5,24 +5,29 @@
 class Nova < Formula
   desc "Check installed Helm charts for updates"
   homepage ""
-  version "2.3.1"
+  version "2.3.2"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/FairwindsOps/nova/releases/download/2.3.1/nova_2.3.1_darwin_amd64.tar.gz"
-    sha256 "078f3834d29a2c57c2c54d1802e78845a7b0157d1aa506e8bd323ece930dccf2"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/FairwindsOps/nova/releases/download/2.3.2/nova_2.3.2_darwin_amd64.tar.gz"
+      sha256 "c3a2e47aa30b26bcd00b6f29fee177b8f25ebe28ff90963f47b18e2c00453e6f"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/FairwindsOps/nova/releases/download/2.3.1/nova_2.3.1_linux_amd64.tar.gz"
-    sha256 "81174d5a85db1c3471cc60733bf337fd9a414f5b2a665cf2d2734897418fc7f2"
-  end
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/FairwindsOps/nova/releases/download/2.3.1/nova_2.3.1_linux_armv6.tar.gz"
-    sha256 "c191c91cbce0357dbaafd6a5af3d6ca1d57e3727c7efd0983d0c4363b6d662b0"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/FairwindsOps/nova/releases/download/2.3.1/nova_2.3.1_linux_arm64.tar.gz"
-    sha256 "0644e722b795b6e8c45d4df54183a3bbf00b29b7a280fc119c69f97b94397af1"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/FairwindsOps/nova/releases/download/2.3.2/nova_2.3.2_linux_amd64.tar.gz"
+      sha256 "8ebbe0ffcf541abd76ee2c46538112b9b9e24612cb7fe5b987e049fadd6c1f9b"
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/FairwindsOps/nova/releases/download/2.3.2/nova_2.3.2_linux_armv6.tar.gz"
+      sha256 "7036c45440e9b089866f30d42130d4c5e78b26270700aaa158e2496ca7796d68"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/FairwindsOps/nova/releases/download/2.3.2/nova_2.3.2_linux_arm64.tar.gz"
+      sha256 "4b1cec4b2f8aa9a213acbec3a1dfc51ec99a91b7450f60204ad890337c043b5c"
+    end
   end
 
   def install
