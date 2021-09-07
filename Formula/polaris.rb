7f62732a589ea1357090133616494af4d3f990be
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-07 08:58:14 -0600

    Brew formula update for polaris version 4.1.0

--- Formula/polaris.rb
+++ Formula/polaris.rb
@@ -2,24 +2,24 @@
 class Polaris < Formula
   desc "Open Source Best Practices for Kubernetes"
   homepage ""
-  version "4.0.9"
+  version "4.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/FairwindsOps/polaris/releases/download/4.0.9/polaris_darwin_amd64.tar.gz"
-    sha256 "a39da7848c034c05bf21fc2fdc3b7e3cc37b0ce45a8ae85911f0c0c4c39a3fec"
+    url "https://github.com/FairwindsOps/polaris/releases/download/4.1.0/polaris_darwin_amd64.tar.gz"
+    sha256 "ac826a30eb317a2d0105dea963481eac7edbbd71c38bde4bc4930059a07fc41b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/FairwindsOps/polaris/releases/download/4.0.9/polaris_linux_amd64.tar.gz"
-      sha256 "eb5932c9b59643604c2a872d298cd106b58cc19494627f55a8897e0c1a1a5e72"
+      url "https://github.com/FairwindsOps/polaris/releases/download/4.1.0/polaris_linux_amd64.tar.gz"
+      sha256 "aa303ecec957f4a1759d4bb1582c900a40ae74ef3aaf09c45f214b66d923d1e0"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/FairwindsOps/polaris/releases/download/4.0.9/polaris_linux_arm64.tar.gz"
-        sha256 "e585d77ac0a126649d61066221427bde5031b4ca2aecfbd112b7496f9b48bf76"
+        url "https://github.com/FairwindsOps/polaris/releases/download/4.1.0/polaris_linux_arm64.tar.gz"
+        sha256 "2e8a960b8ffc9a1f64f86f27ad926c10319d1f671fb3fc218d3b9efa932ee108"
       else
-        url "https://github.com/FairwindsOps/polaris/releases/download/4.0.9/polaris_linux_armv6.tar.gz"
-        sha256 "eb8d794643fa859f3cf1bce38d7d0e38976a5815523749298b29f58a195308b8"
+        url "https://github.com/FairwindsOps/polaris/releases/download/4.1.0/polaris_linux_armv6.tar.gz"
+        sha256 "c5a36d4d91e917b136d708afba8fae48c540217c8fb2c8b4c22d084bdc849b58"
       end
     end
   end
